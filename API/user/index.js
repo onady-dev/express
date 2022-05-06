0bd38126d0d7ee2280cacf7c2d5745222478eeb0
onady <onady.dev@gmail.com>
2022-05-06 15:59:51 +0900

    swagger

--- API/user/index.js
+++ API/user/index.js
@@ -4,5 +4,71 @@ const ctrl = require('./user.ctrl')
 
 router.get("/", ctrl.index);
 router.post("/login", ctrl.login);
+/** 
+* @swagger
+*     components:
+*         schemas:
+*             User:
+*                 type: object
+*                 required:
+*                     - name
+*                 properties:
+*                     id:
+*                         type: integer
+*                         description: 유저 인덱스
+*                     name:
+*                         type: string
+*                         description: 유저 이름.
+*                     createdAt:
+*                         type: string
+*                         format: date
+*                         description: 생성된 날짜.
+*                     updatedAt:
+*                         type: string
+*                         format: date
+*                         description: 수정된 날짜.
+*/
+/**
+*  @swagger
+*  tags:
+*    name: User
+*    description: API to manage your Users.
+*/
+/**
+*  @swagger
+*  paths:
+*   /user:
+*     get:
+*       summary: Lists all the users
+*       tags: [User]
+*       responses:
+*         "200":
+*           description: The list of users.
+*           content:
+*             application/json:
+*               schema:
+*                 $ref: '#/components/schemas/User'
+*/
+/**
+*  @swagger
+*  paths:
+*   /user/login:
+*     post:
+*       summary: Login User
+*       tags: [User]
+*       requestBody:
+*         required: true
+*         content:
+*           application/json:
+*             schema:
+*               $ref: '#/components/schemas/User'
+*       responses:
+*         "200":
+*           description: Login User.
+*           content:
+*             application/json:
+*               schema:
+*                 $ref: '#/components/schemas/User'
+*/
 
 module.exports = router;
